0cf616c4208abc0e0c774ad8e0df969009bdf79b
Administrator <Administrator@DESKTOP-8EUFG1H>
2024-05-09 07:56:28 +0800

    加入專案檔案。

---
+++ prjToDo/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using prjToDo.Models;
+
+namespace prjToDo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        dbToDoEntities db=new dbToDoEntities();
+        public ActionResult Index()
+        {
+            var todos = db.tTodo.OrderByDescending(m=>m.fDate).ToList();
+            return View(todos);
+        }
+    }
+}
