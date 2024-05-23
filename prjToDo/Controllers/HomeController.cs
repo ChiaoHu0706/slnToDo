710be36bda3d1c1515dd3b3a4b41a621d7463732
Administrator <Administrator@DESKTOP-8EUFG1H>
2024-05-23 10:53:02 +0800

    20240523

--- prjToDo/Controllers/HomeController.cs
+++ prjToDo/Controllers/HomeController.cs
@@ -16,5 +16,23 @@ namespace prjToDo.Controllers
             var todos = db.tTodo.OrderByDescending(m=>m.fDate).ToList();
             return View(todos);
         }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create
+           (string fTitle, string fLevel, DateTime fDate)
+        {
+            tTodo todo = new tTodo();
+            todo.fTitle = fTitle;
+            todo.fLevel = fLevel;
+            todo.fDate = fDate.ToString();
+            db.tTodo.Add(todo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
